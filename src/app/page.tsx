585877bff2a5482d3ba2b365d212e24d81e950f9
Bashar <bashar@ieee.org>
2025-02-18 21:14:05 +0400

    PPP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { motion } from 'framer-motion'
 
 export default function Home() {
+  
   return (
     <div className="min-h-screen bg-gradient-to-b from-white to-gray-50">
       {/* Navigation */}

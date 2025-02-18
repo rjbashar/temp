8c550286aa074c835741e77e8f53e642e92807fb
Bashar <bashar@ieee.org>
2025-02-18 21:10:24 +0400

    Remove visitor counter implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import VisitorCounter from "./components/VisitorCounter";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +19,6 @@ export default function RootLayout({
       <body className={`${inter.className} bg-white text-gray-900 scroll-pt-24`}>
         <div className="min-h-screen">
           {children}
-          <div className="fixed bottom-4 right-4">
-            <VisitorCounter />
-          </div>
         </div>
       </body>
     </html>
